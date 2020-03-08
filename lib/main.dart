ab3d4a729ebc66f3710e5145f369d953add20d61
Emam <emamAhmed36@gmail.com>
2020-03-08 15:25:12 +0200

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,23 @@
+import 'package:bloc_builder/counter_page.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+import 'counter_bloc.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Bloc Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: BlocProvider<CounterBloc>(
+        create: (context) => CounterBloc(),
+        child: CounterPage(),
+      ),
+    );
+  }
+}
